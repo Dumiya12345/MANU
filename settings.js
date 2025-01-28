51e4818e6fb47d7386a0132f09713e966e2c8520
Dumiya12345 <dumiyadumiya070@gmail.com>
2025-01-28 23:51:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&4LVVCIzY#TzpxA-7k4KJ7okSif9os4K3HjM4cy9APvQAxlEldVuQ",
 GITHUB_USERNAME: process.env.GITHUB_USERNAME || "put your github usearname",
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "put your github auth token without ghp_",
 };
